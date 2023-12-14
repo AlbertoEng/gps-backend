3d36319ef9b8aca343ee874df236f42390720c03
AlbertoEng <jesyrom0825@gmail.com>
2023-12-13 19:33:55 -0600

    haciendo el pug

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const net = require('net');
 const express = require('express');
-const http = require('http');
+const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
+const port = 3000;
 
-// Ruta para servir la interfaz web estática
-app.use(express.static('public'));
+// Configuración del motor de plantillas Pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// Manejador de conexiones TCP
-server.on('connection', (socket) => {
-  console.log('Cliente TCP conectado');
-
-  // Manejar datos entrantes
-  socket.on('data', (data) => {
-    const rawData = data.toString('utf-8');
-    console.log('Datos crudos recibidos:', rawData);
-    
-    // Puedes emitir los datos a través de WebSockets, por ejemplo
-    io.emit('datos', rawData);
-  });
-
-  // Manejar la desconexión del cliente
-  socket.on('end', () => {
-    console.log('Cliente TCP desconectado');
-  });
-
-  // Manejar errores de conexión
-  socket.on('error', (err) => {
-    console.error('Error de conexión TCP:', err.message);
-  });
+// Ruta de la página principal
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Bienvenido a mi sitio' });
 });
 
-// Configuración del servidor HTTP
-const portHTTP = 3000;
-server.listen(portHTTP, () => {
-  console.log(`Servidor HTTP escuchando en el puerto ${portHTTP}`);
-});
-
-// Configuración de Socket.IO para la comunicación en tiempo real
-const io = require('socket.io')(server);
-
-// Configuración del servidor TCP
-const portTCP = 3001;
-net.createServer((socket) => {
-  server.emit('connection', socket); // Emitir el evento de conexión al servidor HTTP
-}).listen(portTCP, () => {
-  console.log(`Servidor TCP escuchando en el puerto ${portTCP}`);
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
